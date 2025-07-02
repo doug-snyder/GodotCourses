e28058565fee321d317a0451bfc3b8e542f55022
douglas-snyder <mistershake2099@gmail.com>
2025-07-01 23:58:05 -0400

    Final Godot courses begin.

---
+++ introtogodot/Player.gd
@@ -0,0 +1,17 @@
+extends CharacterBody2D
+
+func _physics_process(delta):
+	velocity.x = 0
+	velocity.y = 0
+
+	if Input.is_key_pressed(KEY_LEFT):
+		velocity.x -= 1
+	if Input.is_key_pressed(KEY_RIGHT):
+		velocity.x += 1
+	if Input.is_key_pressed(KEY_UP):
+		velocity.y -= 1
+	if Input.is_key_pressed(KEY_DOWN):
+		velocity.y += 1
+
+	velocity *= 50
+	move_and_slide()
